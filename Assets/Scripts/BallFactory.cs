c11961ee9f7a7a01a7db58fcff18a17ed6852ac7
419087 <111875469+419087@users.noreply.github.com>
2025-11-29 00:24:47 +0900

    feat: 0~4のランダムなサイズのボールが生成されるように変更

--- Assets/Scripts/BallFactory.cs
+++ Assets/Scripts/BallFactory.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class BallFactory : MonoBehaviour
 {
-    [SerializeField] private GameObject _ballPrefab;
+    [SerializeField] private GameObject[] _ballPrefab;
     [SerializeField] private Transform _ballParent;
 
     public GameObject CreateBall()
     {
-        return Instantiate(_ballPrefab, this.transform.position, Quaternion.identity, _ballParent);
+        int ballSize = Random.Range(0, 5);
+        return Instantiate(_ballPrefab[ballSize], this.transform.position, Quaternion.identity, _ballParent);
     }
 }
