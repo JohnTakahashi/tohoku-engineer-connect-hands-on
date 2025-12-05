2a458a26b5d8bac76c6a805f1ee9f09c4a81729c
419087 <111875469+419087@users.noreply.github.com>
2025-12-06 02:24:59 +0900

    feat: スコアを表示するUIと表示ロジックを実装

--- Assets/Scripts/ScoreCalculator.cs
+++ Assets/Scripts/ScoreCalculator.cs
@@ -1,19 +1,28 @@
 using UnityEngine;
+using TMPro;
 
 public class ScoreCalculator : MonoBehaviour
 {
     [SerializeField] private BallMerger _ballMerger;
+    [SerializeField] private TextMeshProUGUI _scoreText; 
+
     private int[] _scoreTable = { 0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 };
     private int _score = 0;
 
     private void Awake()
     {
         _ballMerger.OnMerged += AddScore;
+        UpdateScoreText();
     }
 
     private void AddScore(int mergedBallSize)
     {
         _score += _scoreTable[mergedBallSize];
-        Debug.Log("Score: " + _score);
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        _scoreText.text = "Score: " + _score.ToString("00000");
     }
 }
